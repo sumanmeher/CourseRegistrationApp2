9319b5e9b74e7894017ac1cdb01e59d868e46882
Suman.Meher <Suman.Meher@LDIGITBGL-L0218.godigit.corp>
2023-07-26 18:29:03 +0530

    Merge branch 'main' of https://github.com/sumanmeher/CourseRegistrationApp2.git into main

--- src/main/java/com/digit/javaTraining/CRSApp/Launch.java
+++ src/main/java/com/digit/javaTraining/CRSApp/Launch.java
@@ -6,11 +6,12 @@ public class Launch {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Digital Courses!");
 		System.out.println();
-		
+
 		Admin ad = new Admin();
+		mainMenu(ad);
 
 	}
-	
+
 	static void printMainMenu() {
 		System.out.println("-----------------------------");
 		System.out.println("          MAIN MENU          ");
@@ -18,17 +19,47 @@ public class Launch {
 		System.out.println("1. Admin Login");
 		System.out.println("2. Professor Login");
 		System.out.println("3. Student Login");
-		
+
 	}
-	
+
 	static void mainMenu(Admin ad) {
 		Scanner sc = new Scanner(System.in);
 		printMainMenu();
 		System.out.println("Select an option: ");
 		int userInput = sc.nextInt();
-		if(userInput==1) {
-			boolean isAuth = ad.checkUsernamePassword();
+		if (userInput == 1) {
+			adminAuth(ad);
+
+		} else if (userInput == 2) {
+
+		} else if (userInput == 3) {
+
+		} else if (userInput == 4) {
+			System.out.println("Exiting the Application");
+			System.out.println("Thanks for visiting us..");
+			System.exit(0);
+		} else {
+
 		}
-		
+
+	}
+
+	static void adminAuth(Admin ad) {
+		Scanner sc = new Scanner(System.in);
+		boolean isAuth = ad.checkUsernamePassword();
+		if (isAuth) {
+			System.out.println("Authenticated...");
+		} else {
+			System.out.println("Authentication Failed!");
+			System.out.println();
+			System.out.println("Do you want to try again? Yes/No");
+			String tryAgain = sc.next();
+			if (tryAgain.equalsIgnoreCase("Yes") ) {
+				adminAuth(ad);
+			} else {
+				mainMenu(ad);
+
+			}
+		}
 	}
 }
