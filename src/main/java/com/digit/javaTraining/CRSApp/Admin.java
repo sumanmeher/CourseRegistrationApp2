dbe222347b019e6a64736eca5e839292492757e8
Suman.Meher <Suman.Meher@LDIGITBGL-L0218.godigit.corp>
2023-07-26 21:49:01 +0530

    made changes in courses and admin classes

--- src/main/java/com/digit/javaTraining/CRSApp/Admin.java
+++ src/main/java/com/digit/javaTraining/CRSApp/Admin.java
@@ -5,21 +5,51 @@ import java.util.Scanner;
 public class Admin {
 	String username;
 	String password;
-	
+
 	public Admin() {
-		this.username="admin";
-		this.password="Admin";
+		this.username = "admin";
+		this.password = "Admin";
 	}
-	
+
 	public boolean checkUsernamePassword() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter Admin Username:");
 		String name = sc.next();
 		System.out.println("Enter Admin Password");
 		String pass = sc.next();
-		
-		if(name.equals(username) && pass.equals(this.password))
+
+		if (name.equals(username) && pass.equals(this.password))
 			return true;
 		return false;
 	}
+
+	static void printAdminMenu() {
+		System.out.println("-----------------------------");
+		System.out.println("          ADMIN MENU          ");
+		System.out.println("-----------------------------");
+		System.out.println("1. Add Course");
+		System.out.println("2. Add Professor");
+		System.out.println("3. Add Student");
+		System.out.println("4. Goto Main Menu");
+	}
+
+	static void adminMenu(Admin ad) {
+		Scanner sc = new Scanner(System.in);
+
+		printAdminMenu();
+		System.out.println("Select an option:");
+		int userInput=sc.nextInt();
+		if (userInput == 1) {
+
+		} else if (userInput == 2) {
+
+		} else if (userInput == 3) {
+
+		} else if (userInput == 4) {
+			Launch.mainMenu(ad);
+		} else {
+			return;
+		}
+	}
+
 }
--- src/main/java/com/digit/javaTraining/CRSApp/Course.java
+++ src/main/java/com/digit/javaTraining/CRSApp/Course.java
@@ -1,5 +1,9 @@
 package com.digit.javaTraining.CRSApp;
 
 public class Course {
-
+	String cid;
+	String name;
+	int price;
+	String duration;
+	String description;
 }
